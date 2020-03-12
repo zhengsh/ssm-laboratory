3d8444400a535769dfd6d8db8ff16b7e4036dd6d
382333604@qq.com <xxx@xxx.com>
2020-03-12 14:02:23 +0800

    add spring cloud config

---
+++ payment-service/src/main/java/cn/edu/cqvie/payment/controller/ConfigController.java
@@ -0,0 +1,18 @@
+package cn.edu.cqvie.payment.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ConfigController {
+
+    @Value("${config.info}")
+    private String configInfo;
+
+    @GetMapping("/configInfo")
+    public String getConfigInfo() {
+        return this.configInfo;
+    }
+}
